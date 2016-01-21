ebe9d75cb3a9a29166bb0f3b70aeef7d495a0767
TmCrafz <sterzelTim@gmail.com>
2016-01-21 21:54:08 +0100

    Edit the new Stone and Point classes

--- Stone.cpp
+++ Stone.cpp
@@ -1,4 +1,30 @@
 #ifndef STONE_CPP
 #define STONE_CPP
+#include "Stone.h"
+
+Stone::Stone(Point position, Point subStone1, Point subStone2, 
+	      Point subStone3, Point subStone4):
+	m_position(position)
+{
+	m_subStones[0] = subStone1;
+	m_subStones[1] = subStone2;
+	m_subStones[2] = subStone3;
+	m_subStones[3] = subStone4;
+}
+	
+
+Stone::Stone(int midPointX, int midPointY,
+	      int subStone1X, int subStone1Y,
+	      int subStone2X, int subStone2Y,
+	      int subStone3X, int subStone3Y,
+	      int subStone4X, int subStone4Y)
+{
+	m_position = Point(midPointX, midPointY);
+	m_subStones[0] = Point(subStone1X, subStone1Y);
+	m_subStones[1] = Point(subStone2X, subStone2Y);
+	m_subStones[2] = Point(subStone3X, subStone3Y);
+	m_subStones[3] = Point(subStone4X, subStone4Y);
+}
+
 
 #endif // !STONE_CPP
--- Stone.h
+++ Stone.h
@@ -1,18 +1,39 @@
 #ifndef STONE_H
 #define STONE_H
 #include <vector>
-
+#include "Point.h" 
 /*
- *  The Stones have an own inner coordinate system
+ *  A Stone consists of 4 subStones, represented in Points.
+ *  The Stones have an own inner coordinate system:
+ *          -2
+ *      ....|....
+ *      ....|....
+ *    -4----+----+4
+ *      ....|....
+ *      ....|.... 
+ *          +2
+ *  The "+" is the midpoint relative it is always (0|0),
+ *  but the absolute position depence from the Stone position in the
+ *  Play field.
  */
 
 class Stone
 {
 private:
-	
+	// The position of all substones which result in the whole Stone
+	Point m_subStones[4];
+	// The absolute position of the midpoint
+	Point m_position;
 
 public:
+	Stone(Point position, Point subStone1, Point subStone2, 
+	      Point subStone3, Point subStone4);
 
+	Stone(int midPointX, int midPointY,
+	      int subStone1X, int subStone1Y,
+	      int subStone2X, int subStone2Y,
+	      int subStone3X, int subStone3Y,
+	      int subStone4X, int subStone4Y);
 
 
 	
