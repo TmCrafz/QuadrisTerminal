b37af9aecb11790302a869386a321ab8700b3ea5
TmCrafz <sterzelTim@gmail.com>
2016-01-24 20:27:56 +0100

    Delete test stuff

--- main.cpp
+++ main.cpp
@@ -8,28 +8,6 @@ using namespace std;
 
 int main()
 {
-	char tmp;
-	do 
-	{
-		if (InputHelper::kbhit())
-		{
-			tmp = InputHelper::getch();
-			cout << "Pressed: " << tmp << endl;
-		}
-	} while (tmp != 'k');
-	
-	tmp = ' ';
-	
-
-	do 
-	{
-		if (InputHelper::kbhit())
-			tmp = InputHelper::getch();
-	} while (tmp != 'k');
-
-	 
-
-	cout << "Vim cpp program test, compiled with gcc and make file." << endl;
 	Game game;
 	game.run();
 	
